317a9af3d8c1cb78698c2767ca172c8b7346ca86
Harsha S <sharsha315@gmail.com>
2024-09-15 11:05:02 +0000

    the boilerplate code and files for get started

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API-KEY = os.getenv("")
+
+def main():
+    print("Ok. Done!!!")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
